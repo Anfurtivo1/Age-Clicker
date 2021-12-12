5a32fa6b06e95ed55ac3de8fa0201d4b2cc51164
Shiko <anfurtivo1@hotmail.com>
2021-12-12 07:38:57 +0100

    Cambios casos de prueba
    Se han cambiado los casos de prueba a otros que puedan servir para probar las funcionalidades de los edificios.

--- Assets/Tests/CasosDePrueba.cs
+++ Assets/Tests/CasosDePrueba.cs
@@ -5,69 +5,39 @@ using System.Xml.Serialization;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEditor.SceneManagement;
 
 public class CasosDePrueba
 {
-    //Se comprueba que el archivo de guardado se ha guardado donde se debe de guardar
-    [Test]
-    public void ComprobacionArchivoGuardar()
+
+    [UnityTest]
+    public IEnumerator comprobacionComprarEdificiosAumentoCantidad()
     {
-        string nombreJugador = "Juan";
-        string nombrePartidaEsperado = Application.persistentDataPath+"\\"+nombreJugador+".aock";
+        int cantidadEsperada = 1;
+        int cantidadSacado;
+        EditorSceneManager.LoadScene("Scene 1");
 
+        yield return new WaitForSecondsRealtime(10);
 
-        string[] partidas = Directory.GetFiles(Application.persistentDataPath,nombreJugador+".aock");
+        cantidadSacado = int.Parse(GuardarCargar.instancia.cantidadEdificiosTier1.text);
 
-        Assert.AreEqual(nombrePartidaEsperado, partidas[0]);
-    }
-
-    [Test]
-    public void ComprobacionArchivoCargarNivel()
-    {
-        Juego partidaEsperada = new Juego();
-        Juego partidaRecuperada;
-        string nombreJugador = "Juan";
-
-        //partidaEsperada.nombrePartida = nombreJugador;
-        partidaEsperada.nivelActual = "Scene 1";
-        //partidaEsperada.nivelPrestigio = 1;
-
-        string rutaAGuardar = Application.persistentDataPath + "/" + nombreJugador + ".aock";
-        var serializador = new XmlSerializer(typeof(Juego));
-        FileStream stream = new FileStream(rutaAGuardar, FileMode.Open);
-        partidaRecuperada = serializador.Deserialize(stream) as Juego;
-        stream.Close();
-
-        Assert.AreEqual(partidaEsperada.nivelActual,partidaRecuperada.nivelActual);
+        Assert.AreEqual(cantidadEsperada,cantidadSacado);
 
     }
 
-    [Test]
-    public void ComprobacionArchivoCargarNivelPrestigio()
+    [UnityTest]
+    public IEnumerator comprobacionComprarEdificiosAumentoPrecio()
     {
-        Juego partidaEsperada = new Juego();
-        Juego partidaRecuperada;
-        string nombreJugador = "Juan";
+        int cantidadEsperada = 20;
+        int cantidadSacado;
+        EditorSceneManager.LoadScene("Scene 1");
 
-        partidaEsperada.prestigio = 1;
+        yield return new WaitForSecondsRealtime(10);
 
-        string rutaAGuardar = Application.persistentDataPath + "/" + nombreJugador + ".aock";
-        var serializador = new XmlSerializer(typeof(Juego));
-        FileStream stream = new FileStream(rutaAGuardar, FileMode.Open);
-        partidaRecuperada = serializador.Deserialize(stream) as Juego;
-        stream.Close();
+        cantidadSacado = int.Parse(GuardarCargar.instancia.costeEdifTier1.text);
 
-        Assert.AreEqual(partidaEsperada.prestigio, partidaRecuperada.prestigio);
+        Assert.AreEqual(cantidadEsperada, cantidadSacado);
 
     }
 
-    //// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    //// `yield return null;` to skip a frame.
-    //[UnityTest]
-    //public IEnumerator CasosDePruebaWithEnumeratorPasses()
-    //{
-    //    // Use the Assert class to test conditions.
-    //    // Use yield to skip a frame.
-    //    yield return null;
-    //}
 }
